b5df78e3a45f4d7940393a8aeaf526a246308a31
aoeyofficial <93894861+aoeyofficial@users.noreply.github.com>
2022-10-27 15:48:08 +0700

    first commit

---
+++ cypress/e2e/Test example/Sign in.cy.js
@@ -0,0 +1,12 @@
+describe ('SWAGLABS Test', () => {
+
+  it('Sign in ACC 1' , () => {
+ 
+      beforeEach('Login to the app', () => {
+        cy.loginToApplication()
+      })
+
+    // cy.get('[id="react-burger-menu-btn"]').click()
+
+  })
+})
